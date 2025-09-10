5c1d17591ba6157e03c3d60c9ac641b7843dc3fa
kingslayer-16 <as.aryansharma.16@gmail.com>
2025-09-10 14:18:48 +0530

    Step 10: Add test and implementation for multiple delimiters with [delim1][delim2] format

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -5,14 +5,32 @@ class StringCalculator
     if numbers.start_with?("//")
       # Split once at first newline: delimiter goes left, rest of string goes right
       delimiter_part, numbers_part = numbers.split("\n", 2)
-      delimiter = delimiter_part[2..-1] # Remove "//"
+      delimiter_string = delimiter_part[2..-1] # Remove "//"
 
-      # Handle [delimiter] format for delimiters of any length
-      if delimiter.start_with?("[") && delimiter.end_with?("]")
-        delimiter = delimiter[1..-2] # Remove brackets
+      # Handle multiple delimiters with [delim1][delim2] format
+      if delimiter_string.count("[") > 1
+        # Extract all delimiters between brackets - simple way
+        delimiters = []
+        delimiter_string.split("]").each do |part|
+          if part.include?("[")
+            delimiters << part.split("[")[1]
+          end
+        end
+
+        # Split by each delimiter one by one
+        numbers_array = [numbers_part]
+        delimiters.each do |delim|
+          numbers_array = numbers_array.flat_map { |part| part.split(delim) }
+        end
+        numbers_array = numbers_array.map(&:to_i)
+      elsif delimiter_string.start_with?("[") && delimiter_string.end_with?("]")
+        # Handle single [delimiter] format for delimiters of any length
+        delimiter = delimiter_string[1..-2] # Remove brackets
+        numbers_array = numbers_part.split(delimiter).map(&:to_i)
+      else
+        # Handle simple single character delimiter
+        numbers_array = numbers_part.split(delimiter_string).map(&:to_i)
       end
-
-      numbers_array = numbers_part.split(delimiter).map(&:to_i)
     else
       numbers_array = numbers.split(/[,\n]/).map(&:to_i)
     end
