83bd7adc32542b4ed503756069f6f4d045a732d6
kingslayer-16 <as.aryansharma.16@gmail.com>
2025-09-10 11:32:36 +0530

    Step 2: Add single number test and implementation

--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -5,4 +5,9 @@ class StringCalculatorTest < ActiveSupport::TestCase
     calculator = StringCalculator.new
     assert_equal 0, calculator.add("")
   end
+
+  def test_add_single_number_returns_that_number
+    calculator = StringCalculator.new
+    assert_equal 1, calculator.add("1")
+  end
 end
